4c93838baa2ee6c7b3b7f8c2841e360431b418e8
Andrey Kolotukhin <KolotukhinAndrey@rambler.ru>
2022-03-28 18:30:23 +0300

    Added test

--- src/test/java/ru/netology/test/RegistrationTest.java
+++ src/test/java/ru/netology/test/RegistrationTest.java
@@ -1,4 +1,56 @@
 package ru.netology.test;
 
+import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class RegistrationTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    void shouldSendForm() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $x("//input[@placeholder='Город']").val("Смоленск");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $x("//input[@placeholder='Дата встречи']").val("31.03.2022");
+        $x("//input[@name='name']").setValue("Иванов Иван");
+        $x("//input[@name='phone']").setValue("+79001231212");
+        $(".checkbox__box").click();
+        $(withText("Забронировать")).click();
+        $x("//*[contains(text(),'Успешно!')]").should(appear, Duration.ofSeconds(70));
+    }
 }
