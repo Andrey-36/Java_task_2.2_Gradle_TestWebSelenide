d4a2d70723dabe38354bc08d46b1ac23ca5f7438
Andrey Kolotukhin <KolotukhinAndrey@rambler.ru>
2022-03-29 19:45:31 +0300

    added date generation

--- src/test/java/ru/netology/test/RegistrationTest.java
+++ src/test/java/ru/netology/test/RegistrationTest.java
@@ -2,57 +2,36 @@ package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationTest {
 
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setupAll() {
-        WebDriverManager.chromedriver().setup();
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
-    @BeforeEach
-    void setupTest() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-    }
+    String planningDate = generateDate(5);
 
     @Test
     void shouldSendForm() {
-//        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $x("//input[@placeholder='Город']").val("Смоленск");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $x("//input[@placeholder='Дата встречи']").val("01.04.2022");
+        $("[data-test-id='date'] input").val(planningDate);
         $x("//input[@name='name']").setValue("Иванов Иван");
         $x("//input[@name='phone']").setValue("+79001231212");
         $(".checkbox__box").click();
         $(withText("Забронировать")).click();
-        $x("//*[contains(text(),'Успешно!')]").should(appear, Duration.ofSeconds(70));
+        $("[class='notification__title']").shouldHave(Condition.text("Успешно!"), Duration.ofSeconds(70));
+        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(70));
     }
 }
